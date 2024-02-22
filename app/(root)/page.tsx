2983d17501dcb2a2a82da87093c89a98fafeaef5
jm8gw <82823709+jm8gw@users.noreply.github.com>
2024-02-22 12:37:00 -0500

    Added Clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 // This page ("Home") uses the layout defined in app/(root)/layout.tsx
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl='/' />
+    </div>
   )
 }
 
