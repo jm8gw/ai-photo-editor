c75d7d251e6fa009d69f35a9df75a6d900c1568d
jm8gw <82823709+jm8gw@users.noreply.github.com>
2024-02-29 11:29:01 -0500

    Update price for Pro Package

--- constants/index.ts
+++ constants/index.ts
@@ -81,7 +81,7 @@ export const navLinks = [
       _id: 2,
       name: "Pro Package",
       icon: "/assets/icons/image.svg",
-      price: 0.01,
+      price: 0.50,
       credits: 150,
       inclusions: [
         {
